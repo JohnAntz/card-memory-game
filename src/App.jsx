f748bba6bb936b8ac702bfeff51167fcf553fe7d
JohnAntz <johnantzoulisvazquez@outlook.com>
2025-08-03 16:51:04 +0200

    Render all pokemon cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
+import { useState, useEffect } from "react";
 import Card from "./Components/Card";
 import Score from "./Components/Score";
 
 function App() {
-  const baseUrl = "https://pokeapi.co/api/v2/pokemon-species/1/";
+  const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+  const [record, setRecord] = useState(0);
+  const pokeID = [715, 445, 887, 697, 612, 784, 706, 149, 381, 380, 621, 373];
+  const baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
+
   return (
-    <div className="bg-slate-700 h-dvh w-full flex flex-col items-center  font-poppins">
+    <div className="bg-slate-700 min-h-screen w-full  flex flex-col items-center gap-8 font-poppins">
       <header>
         {" "}
         <h1 className="text-gray-100 text-2xl sm:text-4xl mt-10">
-          Pokemon memory game!
+          Pokemon memory game
         </h1>
         <Score />
       </header>
-      <main>
-        <Card baseUrl={baseUrl} />
+      <main className="grid gap-4 mb-10 sm:grid-cols-3 lg:grid-cols-4">
+        {pokeID.map((id) => (
+          <Card key={id} URL={`${baseUrl}${id}`} pokeID={pokeID} />
+        ))}
       </main>
     </div>
   );
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,29 @@
 import { useState, useEffect } from "react";
 
-export default function Card({ baseUrl }) {
+export default function Card({ URL }) {
+  const [data, setData] = useState(null);
+
   useEffect(() => {
-    fetch(baseUrl)
+    fetch(URL)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setData(data);
+        console.log(data.name);
+        console.log(data.sprites.front_default);
       })
       .catch((err) => {
         console.error("Fetch error:", err);
       });
   }, []);
+
+  return data ? (
+    <div className="flex flex-col items-center bg-slate-200 p-6 rounded-xl border-4 border-slate-400">
+      <img src={data.sprites.front_default} alt="sprite" />
+      <h3 className="text-slate-900 text-xl sm:text-2xl">
+        {data.species.name}
+      </h3>
+    </div>
+  ) : (
+    <div>Loading...</div>
+  );
 }
